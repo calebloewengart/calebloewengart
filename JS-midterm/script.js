61d38f5163f5c2b205d53e838f1ab28950332cd1
caleb.loewengart <calo7523@colorado.edu>
2024-04-28 21:33:51 -0600

    js

--- JS-midterm/script.js
+++ JS-midterm/script.js
@@ -2,6 +2,10 @@ let button = document.querySelector("#vol");
 let text = document.createElement('p');
 let volume = document.createElement('p');
 
+function onSubmit(token) {
+    document.getElementById("demo-form").submit();
+  }
+
 text.textContent = "Click to change volume!";
 button.appendChild(text);
 
@@ -13,3 +17,17 @@ function changeVolume()
 
 changeVolume();
 button.addEventListener('click', changeVolume);
+
+let x = 0;
+
+button.addEventListener('click', ()=>{
+    
+   
+    x++;
+    if (x % 2 === 0 && x % 10 !== 0){
+        alert("Have you found your volume yet?");
+    }
+    if (x % 10 === 0){
+        alert("you're still here?");
+    }
+})
