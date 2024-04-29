8a3611684778cc176779fdc0b4e10283c717c06c
caleb.loewengart <calo7523@colorado.edu>
2024-04-28 21:59:27 -0600

    lol

--- JS-midterm/script.js
+++ JS-midterm/script.js
@@ -1,43 +1,46 @@
 let button = document.querySelector("#vol");
 let text = document.createElement('p');
 let volume = document.createElement('p');
-let requestBody = `
-    {
-    "event": {
-      "token": "TOKEN",
-      "expectedAction": "USER_ACTION",
-      "siteKey": "6LdKTcopAAAAAG5cT8JWqEUUoTvI60TXwcCks-3U",
-    }}`;
-
-const request = JSON.parse(text);
-console.log(request);   
-
-function onSubmit(token) {
-    document.getElementById("demo-form").submit();
-  }
 
 text.textContent = "Click to change volume!";
 button.appendChild(text);
 
 function changeVolume()
 {
+    
     volume.textContent = Math.random()*100;
     button.appendChild(volume);
 }
 
 changeVolume();
-button.addEventListener('click', changeVolume);
 
 let x = 0;
 
 button.addEventListener('click', ()=>{
-    
-   
+    if(x % 7 !== 0){
+       changeVolume(); 
+    }  
+       
     x++;
     if (x % 2 === 0 && x % 10 !== 0){
         alert("Have you found your volume yet?");
     }
     if (x % 10 === 0){
         alert("you're still here?");
+    }
+    if (x % 4 === 0){
+        let prompt = prompt("you good bro? (answer yes or no)", "no");
+        if (prompt.toLowerCase() === "yes"){
+            alert("Glad to hear it :-)");
+        }
+        else
+        {
+            alert("Womp womp, cry about it."); 
+        }
+    }
+    if(x % 7 === 0)
+    {
+        let vulome = prompt("Please enter your volume", "0");
+        volume.textContext = vulome - 20;
     }
 })
